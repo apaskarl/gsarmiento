d1588511815392515272d7285f7f6fb72bc964bb
James Karl Apas <apasjameskarl@gmail.com>
2025-10-03 16:30:17 +0800

    sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://karlapas.vercel.app",
+  siteUrl: "https://gsarmiento.vercel.app",
   generateRobotsTxt: true,
-  sitemapSize: 7000,
+  sitemapSize: 5000,
 };
